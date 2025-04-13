0830d78aab8bf3ae57bf9cef2e4f9e4e763f7507
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2025-04-14 02:59:22 +0530

    feat: implement Apollo Client and GraphQL query for fetching todos refactor: update App component to display todos and handle loading/error states chore: remove unused index.css file chore: add axios dependency for API requests

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useQuery, gql } from "@apollo/client";
 
+const query = gql`
+  query GetAllTodes {
+    getTodos {
+      id
+      title
+      completed
+      user {
+        name
+      }
+    }
+  }
+`;
 function App() {
-  const [count, setCount] = useState(0)
-
+  const { loading, data, error } = useQuery(query);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Todos</h1>
+        {data?.getTodos.map((todo) => (
+          <ul>
+            <li key={todo.id}>{todo.title}</li>
+            <li>{todo.user.name}</li>
+          </ul>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { Suspense } from "react";
 
-createRoot(document.getElementById('root')).render(
+const client = new ApolloClient({
+  uri: "http://localhost:3000/graphql",
+  cache: new InMemoryCache(),
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ApolloProvider client={client}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <App />
+      </Suspense>
+    </ApolloProvider>
+  </StrictMode>
+);
